b73fa848f36d066390c979cdd2f285aa7fd705aa
Daksh-coder-oss <vv0039190@gmail.com>
2024-11-09 11:16:43 +0000

    Add files via upload

---
+++ Divisible.py
@@ -0,0 +1,6 @@
+x=int(input(print("Please enter a number")))
+y=int(input(print("Please enter a number")))
+if x%y==0:
+    print("X is divisible by y")
+else:
+    print("X is not divisible by y")    
---
+++ Operators.py
@@ -0,0 +1,17 @@
+a=5
+b=7
+c=2
+d=8
+e=9
+f=a-b*c/d+e
+print("value of a-b*c/d+e",f)
+
+
+name="Daksh"
+age=49
+if name=="Daksh" or name=="Keyleon" and age>=0:
+    print("Happy Birthday")
+else:
+    print("Bye bye")    
+
+
